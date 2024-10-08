a3d1a4240a2e464d8d1980ae0481718579ff1c70
TeoRamosKees <teokpo@hotmail.com.ar>
2024-10-08 16:25:53 -0300

    dynamic caption

--- s3nextapp/src/app/addPost/page.tsx
+++ s3nextapp/src/app/addPost/page.tsx
@@ -1,14 +1,19 @@
 "use client";
+import { useState } from "react";
 import { addPost } from "../actions";
 
 export default function AddPost() {
+  const [caption, setCaption] = useState("");
+  const [file, setFile] = useState("");
+
   async function submitPost() {
     let post = {
       image_url: "Ejemplo1",
-      image_caption: "Ejmlo",
+      image_caption: caption,
       image_name: "prueba",
       created: new Date().toISOString(),
     };
+    setCaption("");
 
     await addPost(post);
   }
@@ -19,12 +24,21 @@ export default function AddPost() {
         className="grid grid-cols-1  p-4 w-fit border border-slate-800 rounded-md m-5 gap-4"
         onSubmit={submitPost}
       >
-        <input name="image" className="cursor-pointer" type="file" />
+        <input
+          name="image"
+          className="cursor-pointer"
+          type="file"
+          onChange={(e) => setFile(e.target.value)}
+          required
+        />
         <input
           className="border border-black rounded-md pt-1 pl-1"
           type="text"
           placeholder="Add caption..."
           name="caption"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+          required
         />
         <div className="grid justify-items-end">
           <input
